2d6d36f3a983fc9e68b85be3c3149b87df172dfb
Bobby Coleman <colemankidding@gmail.com>
2025-03-31 23:57:35 -0400

    Update for Heroku deployment with dynamic port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 4040;
+const port = process.env.PORT || 3030;
 
 // Serve static files from the root directory
 app.use(express.static('./'));
